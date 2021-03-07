1d3733f42b9745dcbb736713ed65b43cc72e4ff5
BackRunner <dev@backrunner.top>
2021-03-07 19:15:23 +0800

    update: add a new mode to modify the existed resources

--- ResourceMod.cs
+++ ResourceMod.cs
@@ -5,24 +5,61 @@ using BepInEx.Configuration;
 
 namespace DSPResourceMod
 {
-  [BepInPlugin("top.backrunner.DSP.ResourceMod", "DSP Resource Mod", "1.0")]
+  [BepInPlugin("top.backrunner.DSP.ResourceMod", "DSP Resource Mod", "1.1")]
   public class ResourceMod : BaseUnityPlugin
   {
     ConfigEntry<KeyCode> multiplierModHotKeyConfig, existedResourceModHotKeyConfig;
     ConfigEntry<float> multiplierRateConfig;
     ConfigEntry<bool> autoReloadConfig;
+    ConfigEntry<bool> modExistedResourceByMultiplier;
     ConfigEntry<int> singleVeinAmountConfig;
+    ConfigEntry<bool> isDebug;
+
+    float modRate = 0;
 
     void modifyMultiplierRate()
     {
+      if (DSPGame.GameDesc == null)
+      {
+        return;
+      }
       GameSave.SaveCurrentGame("_before_mutiplier_mod_" + DateTime.Now.ToString("s"));
+      modRate = multiplierRateConfig.Value / DSPGame.GameDesc.resourceMultiplier;
       DSPGame.GameDesc.resourceMultiplier = multiplierRateConfig.Value;
       GameSave.SaveCurrentGame("_after_multiplier_mod_" + DateTime.Now.ToString("s"));
-      UIMessageBox.Show("DSPResourceMod", "资源倍率修改成功，当前倍率：" + multiplierRateConfig.Value.ToString() + "。你可以在存档里找到修改前和修改后的存档。", "确定", 0);
+      Language language = Localization.language;
+      if (language == Language.zhCN)
+      {
+        UIMessageBox.Show("DSPResourceMod", "资源倍率为修改成功，当前资源倍率为" + multiplierRateConfig.Value.ToString() + "。你可以在存档列表里找到修改前和修改后的存档。", "确定", 0);
+      }
+      else
+      {
+        UIMessageBox.Show("DSPResourceMod", "Multipler modification completed, current rate: " + multiplierRateConfig.Value.ToString() + ". You can find the pre-modified and post-modified savegame in the savegames.", "Confirm", 0);
+      }
     }
     void modifyExistedResource()
     {
+      if (DSPGame.GameDesc == null)
+      {
+        return;
+      }
+      if (modRate == 0)
+      {
+        modRate = multiplierRateConfig.Value / DSPGame.GameDesc.resourceMultiplier;
+      }
+      if (modRate == 0 || modRate == 1)
+      {
+        if (Localization.language == Language.zhCN)
+        {
+          UIRealtimeTip.Popup("存档资源倍率未改动，资源量无需修改.");
+        } else
+        {
+          UIRealtimeTip.Popup("There is no need to modify the resource because the resource multiplier rate is not changed");
+        }
+        return;
+      }
       GameSave.SaveCurrentGame("_before_resource_mod_" + DateTime.Now.ToString("s"));
+      UIRealtimeTip.Popup("Game saved before modifying.");
       foreach (PlanetFactory factory in GameMain.data.factories)
       {
         if (factory == null)
@@ -35,13 +72,26 @@ namespace DSPResourceMod
         {
           continue;
         }
-        for (int i = 0; i < veins.Length; i++)
+        if (isDebug.Value)
+        {
+          UIRealtimeTip.Popup("Current mod rate: " + modRate.ToString());
+        }
+          for (int i = 0; i < veins.Length; i++)
         {
           if (veins[i].id != 0 && veins[i].type != EVeinType.Oil)
           {
-            veins[i].amount = singleVeinAmountConfig.Value;
+            if (modExistedResourceByMultiplier.Value)
+            {
+              veins[i].amount = (int)(veins[i].amount * modRate);
+            }
+            else
+            {
+              veins[i].amount = singleVeinAmountConfig.Value;
+            }
           }
         }
+        // reset mod rate
+        modRate = 0;
         // update planet data
         PlanetData.VeinGroup[] veinGroups = factory.planet.veinGroups;
         long[] veinAmounts = new long[factory.planet.veinAmounts.Length];
@@ -61,23 +111,36 @@ namespace DSPResourceMod
       }
       string saveGameName = "_after_resource_mod_" + DateTime.Now.ToString("s");
       GameSave.SaveCurrentGame(saveGameName);
+      UIRealtimeTip.Popup("Game saved after modifying.");
       if (autoReloadConfig.Value)
       {
+        UIRealtimeTip.Popup("Auto reload game.");
         DSPGame.StartGame(saveGameName);
-        UIMessageBox.Show("DSPResourceMod", "资源修改成功，你可以在存档里面找到修改前和修改后的存档。" + multiplierRateConfig.Value.ToString(), "确定", 0);
+        Language language = Localization.language;
+        if (language == Language.zhCN)
+        {
+          UIMessageBox.Show("DSPResourceMod", "资源修改成功，你可以在存档里面找到修改前和修改后的存档。", "确定", 0);
+        }
+        else
+        {
+          UIMessageBox.Show("DSPResourceMod", "The resources are modified successfully, you can find the savegame before and after the modifications in the savegame list.", "确定", 0);
+        }
+
       }
       else
       {
-        UIMessageBox.Show("DSPResourceMod", "资源修改成功，你可以在存档里面找到修改前和修改后的存档。为避免游戏出错，我们建议加载一次修改后的存档。" + multiplierRateConfig.Value.ToString(), "确定", 0);
+        UIMessageBox.Show("DSPResourceMod", "The resources are modified successfully, you can find the savegame before and after the modifications in the savegame list.\nTo avoid errors in the game, we recommend loading the modified savegame once.", "确定", 0);
       }
     }
     void Start()
     {
-      existedResourceModHotKeyConfig = Config.Bind("HotKey", "ModifyExistedResource", KeyCode.F7, "已存在资源改成无限");
-      multiplierModHotKeyConfig = Config.Bind("HotKey", "ModifyResourceMultiplier", KeyCode.F8, "修改资源倍率按键");
-      multiplierRateConfig = Config.Bind("Options", "MultipilerRate", 100f, "资源倍率修改");
-      autoReloadConfig = Config.Bind("Options", "AutoReload", true, "自动保存存档并读取");
-      singleVeinAmountConfig = Config.Bind("Options", "SingleVeinAmount", 100000000, "单个矿的数量");
+      existedResourceModHotKeyConfig = Config.Bind("HotKey", "Modify existed resource", KeyCode.F7, "已存在资源改成无限");
+      multiplierModHotKeyConfig = Config.Bind("HotKey", "Modify resource multiplier", KeyCode.F8, "修改资源倍率按键");
+      multiplierRateConfig = Config.Bind("Options", "Multipiler rate", 100f, "资源倍率修改");
+      autoReloadConfig = Config.Bind("Options", "Auto reload", true, "自动保存存档并读取");
+      singleVeinAmountConfig = Config.Bind("Options", "Single vein amount", 100000000, "单个矿的数量");
+      modExistedResourceByMultiplier = Config.Bind("Options", "Use modified mulitplier value to modify vein amount", false, "是否使用修改的资源倍率修改现有矿物");
+      isDebug = Config.Bind("Debug", "Open debug messages", false, "打开调试信息");
     }
     void Update()
     {
